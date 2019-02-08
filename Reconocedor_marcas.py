c1fbb54963a9f459517369c36a9f9a5f9c38d9b0
Juan Carlos Coba López <pruemabe@gmail.com>
2019-02-08 18:41:56 -0400

    Actualizacion codigo de rutina sin nada relevante, solo enrutado
    enrutado de imagenes y librerias

--- Reconocedor_marcas.py
+++ Reconocedor_marcas.py
@@ -1,11 +1,13 @@
 from difflib import SequenceMatcher as SM
+from PIL import Image
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+im = Image.open("pruebaTUBO.jpg")
+texto = pytesseract.image_to_string(im)
+print(texto)
 
 s1 = 'PHILIPS'
-s2 = '"%/&&/PHILIPS2$'
+s2 = texto
 print(SM(None, s1, s2).ratio())
-
-s1 = 'PHILIPS'
-s2 = '"%/&&/PHILSZ<$'
-print(SM(None, s1, s2).ratio())
-
-
