076923ed2fa57ae8d83a0879b70796217187d240
Juan Carlos Coba López <pruemabe@gmail.com>
2019-02-10 19:20:25 -0400

    Importación de librerias y pruebas iniciales terminadas
    Se termino de integrar todo lo necesario para el reconocimiento, faltaria la limpieza pero eso depende de la manera de captura que realizarán. También falta diseñar el algoritmo de discriminación de marcas, algo sencillo de realizar.

--- Reconocedor_marcas.py
+++ Reconocedor_marcas.py
@@ -1,13 +1,15 @@
+#  -*- coding: utf-8 -*-
 from difflib import SequenceMatcher as SM
 from PIL import Image
 import pytesseract
-
-
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+#tessdata_dir_config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+#im = Image.open("test.png")
 im = Image.open("pruebaTUBO.jpg")
+#im = Image.open("pruebaTextoBN.jpg")
+#im = Image.open("pruebaTexto.jpg")
 texto = pytesseract.image_to_string(im)
-print(texto)
-
+#print(texto.encode("utf-8"))
 s1 = 'PHILIPS'
 s2 = texto
 print(SM(None, s1, s2).ratio())
